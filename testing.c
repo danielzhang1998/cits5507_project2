0fd8ec806e28d15145416e25c1e8fe811d40cd77
danielzhang1998 <63471153+danielzhang1998@users.noreply.github.com>
2021-09-26 19:28:53 +0800

    mpi version of binary file read and write

--- testing.c
+++ testing.c
@@ -1,26 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <mpi.h>
+
 #include "binary.h"
-#include "tools.h"
 #include "enum.h"
 #include "merge.h"
+#include "mpi_binary.h"
 #include "quick.h"
+#include "tools.h"
 
 
-#define N 100
+#define N 10
 
-int main(){
+int main(int argc, char **argv){
     double *array = generate_array(N);
     save_binary("unsorted_array", array, N);
     //printf("fuck\n");
     double *new_array = read_binary("unsorted_array", N);
 
-/*
-    for (int j = 0; j < N; j++){
-        printf("%f\n", new_array[j]);
-    }
-*/
+    print_array(new_array, N);
+
     check_binary(array, new_array, N);
+
+    printf("\n===================================================\n");
+    //===================================================//
+
+    int rank, size;
+
+    
+    MPI_Init(0, 0);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    mpi_write("mpi_unsorted_array", array, N, rank);
+
+    double *mpi_new_array = mpi_read("mpi_unsorted_array", N, rank);
+
+    print_array(mpi_new_array, N);
+
+    check_binary(array, mpi_new_array, N);
+
+    MPI_Finalize();
+
     return 0;
 }
